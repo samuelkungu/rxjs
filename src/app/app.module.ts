683e8161dc192db78bf8369f38bc90622de843c8
Samuel <kungusamuel64@gmail.com>
2022-07-22 12:12:11 +0300

    ⬆️ use mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
-import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
+
   ],
   imports: [
     BrowserModule
